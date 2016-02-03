f73ed9f065a1cbe3f76dcfa8ca90ff826711d9b9
Martin Kolb <martin.kolb@mkolb.de>
2016-02-03 14:41:39 +0100

    new website init commit

---
+++ new/js/creative.js
@@ -0,0 +1,70 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+(function($) {
+
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '55px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+    // custom stuff
+    var custom = {
+        init: function() {
+
+            this.fetchData();
+
+        },
+
+        fetchData: function(){
+            $.ajax({
+                method: 'GET',
+                url: 'https://api.meetup.com/2/events?key=XXX&sign=true&group_urlname=web-and-wine&status=upcoming,past'
+            })
+            .done(function(response) {
+                console.log(response);
+            });
+        }
+    };
+
+    custom.init();
+
+})(jQuery); // End of use strict
