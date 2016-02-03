7a7c7a6e07d3a938416a17663d41442baad22e7e
Martin Kolb <martin.kolb@mkolb.de>
2016-02-03 17:24:50 +0100

    api stuff

--- new/js/creative.js
+++ new/js/creative.js
@@ -41,7 +41,7 @@
         offset: {
             top: 100
         }
-    })
+    });
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
@@ -50,17 +50,76 @@
     var custom = {
         init: function() {
 
-            this.fetchData();
+            this.getAllEvents();
+            this.getAllImages();
+            this.getGroupInfos();
+            this.getGroupComments();
 
         },
 
-        fetchData: function(){
+        getAllEvents: function(){
+
             $.ajax({
-                method: 'GET',
-                url: 'https://api.meetup.com/2/events?key=XXX&sign=true&group_urlname=web-and-wine&status=upcoming,past'
-            })
-            .done(function(response) {
-                console.log(response);
+                dataType:'jsonp',
+                method:'get',
+                url: 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=web-and-wine&page=200&fields=&order=time&status=upcoming%2Cpast&desc=false&sig_id=101494212&sig=51e0751f14b33368c235ac3686e5d425ea2b236a',
+                success: function(response) {
+                    console.log(response);
+                    var nextOrLastMeetup = $('#next-or-last-meetup');
+                    var meetupDate = $('#meetup-date');
+                    var linkMeetup = $('#link-meetup');
+                    
+                    var date = new Date(response.results[response.results.length - 1].time);
+                    var day = date.getDate();
+                    var month = parseInt(date.getMonth()) + 1;
+                    var year = date.getFullYear();
+                    var hour = date.getHours();
+                    var minutes = date.getMinutes();
+
+                    nextOrLastMeetup.html('Web&Wine  – Ort: ' + response.results[response.results.length - 1].venue.name);
+                    meetupDate.text(day + '.' + month + '.' + year + ', ' + hour + ':' + minutes + ' Uhr');
+                    linkMeetup.html('<a href="' + response.results[response.results.length - 1].event_url + '" target="_blank"class="btn btn-primary btn-xl page-scroll">Anmeldung über Meetup</a>');
+                    
+                }
+            });
+        },
+
+        getAllImages: function() {
+
+            $.ajax({
+                dataType:'jsonp',
+                method:'get',
+                url: 'https://api.meetup.com/2/photos?offset=0&format=json&group_urlname=web-and-wine&page=200&fields=&order=time&desc=True&sig_id=101494212&sig=b13d93e55d602ec1529b6d06d338454aa16ebcd8',
+                success: function(response) {
+                    console.log(response);
+
+
+
+                }
+            });
+        },
+
+        getGroupInfos: function() {
+
+            $.ajax({
+                dataType:'jsonp',
+                method:'get',
+                url: 'https://api.meetup.com/2/groups?offset=0&format=json&group_urlname=web-and-wine&page=200&radius=25.0&fields=&order=id&desc=false&sig_id=101494212&sig=062a2554098d8bcc7892aba3e595dfe690fc5ea7',
+                success: function(response) {
+                    console.log(response);
+                }
+            });
+        },
+
+        getGroupComments: function() {
+
+            $.ajax({
+                dataType:'jsonp',
+                method:'get',
+                url: 'https://api.meetup.com/comments?offset=0&format=json&group_urlname=web-and-wine&page=200&order=ctime&sig_id=101494212&sig=3079f7184943cfdcfa4982b566c6751183e345af',
+                success: function(response) {
+                    console.log(response);
+                }
             });
         }
     };
